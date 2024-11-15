308217aa6356875dbd8c248dfab9252a9ea00c25
Alex <z1710055859@gmail.com>
2024-11-15 18:54:00 +0100

    o

---
+++ Practica3/Jiabo_zhan/utilstemplate.c
@@ -0,0 +1,86 @@
+#include "utils.h"
+
+int no_deleted_registers = NO_DELETED_REGISTERS;
+
+bool check_dat(const char* filename) {
+    size_t len = strlen(filename);
+    if(filename == NULL || !(filename[len-3] == 'd' && filename[len-2] == 'a' && filename[len-1] == 't'))
+        return false;
+    
+    return true;
+}
+void replaceExtensionByIdx(const char *fileName, char * indexName) {
+    size_t len = strlen(fileName);
+
+    strcpy(indexName,fileName);
+
+    indexName[len-3] = 'i';
+    indexName[len-2] = 'd';
+    indexName[len-1] = 'x';
+
+    return;
+}
+
+
+bool createTable(const char * tableName) {
+    FILE *f;
+    int a = -1;
+    
+    if(check_dat(tableName) == false)
+        return false;
+    
+    if(f = fopen(tableName,"rb") == NULL) {
+        if((f = fopen(tableName,"wb")) == NULL)
+            return false;
+        
+        fwrite(&a,sizeof(int),1,f );
+        fclose(f);
+        return createIndex(tableName);
+    }
+    return true;
+}
+
+bool createIndex(const char *indexName) {
+    FILE *f;
+    int a = -1;
+    char *indexName_Rep = (char*) malloc((strlen(indexName)+1)* sizeof(char));
+
+    replaceExtensionByIdx(indexName,indexName_Rep);
+
+    if(indexName_Rep == NULL)
+        return false;
+    if(f = fopen(indexName_Rep,"rb") == NULL) {
+        if((f = fopen(indexName_Rep,"wb")) == NULL)
+            return false;
+        
+        fwrite(&a,sizeof(int),2,f );
+        fclose(f);
+        free(indexName_Rep);
+    }
+    return true;
+}
+void printnode(size_t _level, size_t level,
+               FILE * indexFileHandler, int node_id, char side) {
+                
+    return;
+}
+
+void printTree(size_t level, const char * indexName)
+{
+    return;
+}
+
+bool findKey(const char * book_id, const char *indexName,
+             int * nodeIDOrDataOffset)
+ {
+     return true;
+ }
+
+bool addIndexEntry(char * book_id,  int bookOffset, char const * indexName) {
+    return true;
+}
+
+bool addTableEntry(Book * book, const char * dataName,
+                   const char * indexName) {
+    return true;
+}
